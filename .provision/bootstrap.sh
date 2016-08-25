7f2e3b09ba6f9cfb1c9b48acc9e1ef60a8978f1c
Jason Favors <jason@jasonfavors.com>
2016-08-24 21:48:05 -0400

    Update SSL configs for multiple certs
    * Generate a dummy cert for example.com
    * Update http and https default server name to use IP

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -20,6 +20,7 @@ yum -y install mod_ssl
 sed -i -e 's/SSLProtocol all -SSLv2$/SSLProtocol -SSLv2 -SSLv3 \+TLSv1 \+TLSv1.1 \+TLSv1.2/g' /etc/httpd/conf.d/ssl.conf
 sed -i -e 's/SSLCipherSuite HIGH:MEDIUM:!aNULL:!MD5:!SEED:!IDEA$/SSLCipherSuite ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA/g' /etc/httpd/conf.d/ssl.conf
 sed -i -e 's/#SSLHonorCipherOrder on $/SSLHonorCipherOrder on/g' /etc/httpd/conf.d/ssl.conf
+sed -i -e 's/#ServerName www.example.com:443$/ServerName 192.168.33.10:443/g' /etc/httpd/conf.d/ssl.conf
 
 echo "Setting up database server"
 yum -y install mariadb-server
@@ -49,9 +50,28 @@ echo "Setting up admin tools"
 # yum install -y phpMyAdmin
 
 echo "Setting up projects"
+cp /vagrant/.provision/apache/000-default.conf /etc/httpd/conf.d/
+
+# Example.com
 mkdir -p /var/www/example.com
 mkdir -p /etc/httpd/logs/example.com
-cp /vagrant/.provision/apache/*.conf /etc/httpd/conf.d/
-cp -R /vagrant/* /var/www/example.com
-# chcon -R -t httpd_sys_content_t /var/www/example.com/html
+cp /vagrant/.provision/apache/example.com.conf /etc/httpd/conf.d/
+cp -R /vagrant/html /var/www/example.com
+
+answers() {
+	echo --
+	echo SomeState
+	echo SomeCity
+	echo SomeOrganization
+	echo SomeOrganizationalUnit
+	echo example.com
+	echo root@example.com
+}
+
+answers | openssl req -newkey rsa:2048 -keyout /etc/pki/tls/private/example.com.key -nodes -x509 -days 365 -out /etc/pki/tls/certs/example.com.crt 2> /dev/null
+
+chown root:root /etc/pki/tls/private/example.com.key
+chown root:root /etc/pki/tls/certs/example.com.crt
+chmod 600 /etc/pki/tls/private/example.com.key
+chmod 600 /etc/pki/tls/certs/example.com.crt
 service httpd restart
